0acbb0f97e59fe9af3ab4e43cab6177d5ef65c50
cllewell <26370356+cllewell@users.noreply.github.com>
2026-02-12 03:04:20 -0500

    genealogy project upload
    genealogy project upload

---
+++ Genealogy Research Course/story_content/triggers.js
@@ -0,0 +1,215 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5l98TTImY0A":
+        Script1();
+        break;
+      case "5ZYClz01VRD":
+        Script2();
+        break;
+      case "6IlhVvK0Xrd":
+        Script3();
+        break;
+      case "6KcjI567cPS":
+        Script4();
+        break;
+      case "6lIzUp1FJia":
+        Script5();
+        break;
+      case "5my96Z6MzVf":
+        Script6();
+        break;
+      case "61FSmdsp0FJ":
+        Script7();
+        break;
+      case "6dNjoM6iudk":
+        Script8();
+        break;
+      case "5wtm8dmDrdt":
+        Script9();
+        break;
+      case "6n0MfFxp0j6":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6OAWbdxjm7I');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5cfEwNDH5Bi';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6hLfbZIKU5h');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Nj1r2ajVmL';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6CuWrMMbMED');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5buMwWSpNoQ';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('68mpx8YHlGg');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6ZISSPRkEOm';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('6PZu61zogY5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6F0wlouKdOy';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('6cKLQlqTQ5Q');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5cfEwNDH5Bi';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script7 = function()
+{
+  const target = object('6Eo88q6dbJA');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Nj1r2ajVmL';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script8 = function()
+{
+  const target = object('6kNEjc0T7Xm');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5buMwWSpNoQ';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script9 = function()
+{
+  const target = object('5xoYK6e2sPf');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6F0wlouKdOy';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script10 = function()
+{
+  const target = object('5iWqiuyG3wn');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5oo7tjELPmo';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: `${1 + growAmount}` }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
